6bf41c5c3aa942c1f7716c44a73bf1a28155c49b
AXubuzi <102726899+AXubuzi@users.noreply.github.com>
2022-05-12 12:42:46 +0200

    first commit

---
+++ Mobile App/Mobile App/Mobile App/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            NavigationPage navigationPage = new NavigationPage(new MainPage());
+            navigationPage.BarBackgroundColor = Color.Black;
+            MainPage = navigationPage;
+        }
+
+       
+    }
+}
---
+++ Mobile App/Mobile App/Mobile App/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobile_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginPage());
+        }
+
+        private void HomePage_Chicked(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+    }
+    
+  
+}
